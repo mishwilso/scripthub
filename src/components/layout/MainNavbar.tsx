cd95e1219f071b502535da21f313383b442ffb5e
Mish Wilson <mishwilsonk@gmail.com>
2025-11-16 14:56:07 -0500

    Create base page routes and main page nav bar

---
+++ src/components/layout/MainNavbar.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Link from 'next/link'
+
+
+export default function MainNavbar() {
+    
+
+    return (
+        <nav>
+            This is Nav!
+        </nav>
+    )
+}
